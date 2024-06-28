3a7a97c5a3a98d04b67da47fde6373b6d6881c79
KHUSHI LODHI <90182941+KHUSHII11@users.noreply.github.com>
2024-06-28 16:47:44 +0530

    Add files via upload

---
+++ routes/users.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const plm = require('passport-local-mongoose');
+
+mongoose.connect(process.env.MONGODB_URI,{});
+
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  email: String,
+  password: String,
+  profileImage: String,
+  contact: Number,
+  board: {
+    type: Array,
+    default: []
+  },
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'post'
+    }
+  ]
+})
+userSchema.plugin(plm);
+module.exports = mongoose.model('user', userSchema);
